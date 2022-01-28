3d158de9898702bb2cceb5b6e1028aa45d61e3ff
MahmoudNoureddine <mahmoudnourdein@gmail.com>
2022-01-28 13:01:23 +0100

    login page and details logo margin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Details from './components/Details';
+import Login from './components/Login';
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,12 +17,16 @@ function App() {
       <Router>
         <Header />
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>  
           <Route path="/details">
             <Details />
           </Route>
           <Route path="/">
           <Home />
           </Route>
+          
         </Switch>
       </Router>
         
